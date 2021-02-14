c9b10f759db1bd2033c115469fa5b4ba712093d7
tolgabirdal <tolga.birdal@tum.de>
2021-02-14 10:51:47 -0800

    no message

--- test_affsub_lq_weisz.m
+++ test_affsub_lq_weisz.m
@@ -37,7 +37,7 @@ x0 = x0./nLines;
 hold on, plot(x0(1), x0(2), 'b+');
 x0 = [-0.5; 0.5]+2;
 projFunc = @(x)(x);
-q = 1.0;
+q = 1.01;
 iterations = 30;
 
 w = ones(nLines, 1);
@@ -46,10 +46,10 @@ hold on, plot(xNormal(1), xNormal(2), 'ko');
 
 for trials = 1:1000
     Ak = A; Ck = C;
-    Ak{1} = Ak{1}+0.75*randn(2, 2);
-    Ck{1} = Ck{1}+0.75*randn(2, 1);
-    Ak{5} = Ak{5}+0.75*randn(2, 2);
-    Ck{5} = Ck{5}+0.75*randn(2, 1);
+    Ak{1} = Ak{1}+0.275*randn(2, 2);
+    Ck{1} = Ck{1}+0.275*randn(2, 1);
+    Ak{5} = Ak{5}+0.275*randn(2, 2);
+    Ck{5} = Ck{5}+0.275*randn(2, 1);
     [X] = AffSub_Lq_weiszfeld(Ak, Ck, x0, q, iterations, projFunc);
     if (norm(X)<10)
         hold on, plot(X(1), X(2), 'ro');
